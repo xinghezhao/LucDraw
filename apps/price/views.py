6af8c42640a018988fb4b7195e05d0881efebda7
xinghezhao <xinghezhao@163.com>
2017-01-29 18:36:56 +0800

    Initial commit

---
+++ apps/price/views.py
@@ -0,0 +1,44 @@
+# _*_ coding:utf-8 _*_
+from django.shortcuts import render
+
+from .models import UserMessage
+from .forms import UploadImageForm
+
+# Create your views here.
+
+def getform(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        message = request.POST.get('message', '')
+        address = request.POST.get('address', '')
+
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.email = email
+        user_message.message = message
+        user_message.address = address
+        #保存图片到数据库并且到相应文件夹
+        image_form = UploadImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            headImg = image_form.cleaned_data['image']
+            user_message.image = headImg
+        user_message.save()
+
+
+
+        # image_form = UploadImageForm(request.POST, request.FILES)
+        # if image_form.is_valid():
+        #     headImg = image_form.cleaned_data['image']
+        #     img = UserMessage()
+        #     img.image = headImg
+        #     img.save()
+
+
+
+
+    return render(request, 'message-form.html',{})
+
+
+
