bb0d1492fd51d5b58b3393bb7ea6d6f5730c9f92
xinghezhao <xinghezhao@163.com>
2017-02-04 16:04:35 +0800

    前台图片路径问题

--- LucDraw/urls.py
+++ LucDraw/urls.py
@@ -19,12 +19,13 @@ from django.contrib import admin
 #from django.views.static import serve  #用于处理上传静态文件
 #from LucDraw.settings import MEDIA_ROOT
 
-from price.views import getform,edit_favorites
+from price.views import getform,edit_favorites,favorites
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^form/$', getform, name='go_form'),
-    url(r'^edit_favorites/', edit_favorites),
+    url(r'^edit_favorites/', edit_favorites, name='edit_favorites'),
+    url(r'^favorites/', favorites, name='favorites'),
 
     # 配置上传文件的访问函数
     #url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
--- apps/price/views.py
+++ apps/price/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse #指定返回给用户的类型
 
 from .models import UserMessage
 from .forms import UploadImageForm
+import json
 
 
 # Create your views here.
@@ -29,27 +30,35 @@ def getform(request):
         user_message.save()
 
 
-
-        # image_form = UploadImageForm(request.POST, request.FILES)
-        # if image_form.is_valid():
-        #     headImg = image_form.cleaned_data['image']
-        #     img = UserMessage()
-        #     img.image = headImg
-        #     img.save()
-
-
-
-
     return render(request, 'message-form.html',{})
+
+
+
 
 def edit_favorites(request):
     if request.is_ajax():
-        message = "Yes, AJAX!"
+        aList = []
+        if request.method == 'GET':
+            message = "Yes, AJAX is get!"
+        elif request.method == 'POST':
+            headphotos = UserMessage.objects.all()
+            for headphoto in headphotos:
+                try:
+                    aList.append(headphoto.image.url)
+                    print aList
+                except:
+                    continue
     else:
         message = "Not Ajax"
-    return HttpResponse(message)
+    return HttpResponse(json.dumps(aList), content_type="application/json")
 
-    #return render(request, 'index.html', {})
+
+
+
+def favorites(request):
+
+    return render(request,'index.html',{})
+
 
 
 
